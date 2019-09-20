e2f7e2a5ea8fa6378cc8ea62866435baedbe6068
Elsa Pioli <elsa.twanss@gmail.com>
2019-09-20 12:00:08 +0200

    inclure php dans homeview

--- vues/homeview.php
+++ vues/homeview.php
@@ -17,65 +17,36 @@
   </header>
 
   <!-- Main Content -->
+ <?php
+        while ($donnees = $req->fetch())
+        {
+        ?>
+
   <div class="container">
     <div class="row">
       <div class="col-lg-8 col-md-10 mx-auto">
         <div class="post-preview">
-          <a href="post.html">
+          <a href="vues/articleview.php">
             <h2 class="post-title">
-              Man must explore, and this is exploration at its greatest
+              <?php echo htmlspecialchars($donnees['title']); ?>
             </h2>
             <h3 class="post-subtitle">
-              Problems look mighty small from 150 miles up
+              <?php echo htmlspecialchars($donnees['content']); ?> <em><a href="<?='index.php?page=article&id='.$donnees->getID() ?>">...[lire la suite]</a>
             </h3>
           </a>
-          <p class="post-meta">Posted by
-            <a href="#">Start Bootstrap</a>
-            on September 24, 2019</p>
+          <p class="post-meta">Posté par
+            <a href="#">Jean Forteroche</a>
+            le <?php echo $donnees['date']; ?> :</p>
         </div>
+          <?php
+        }
+        $req->closeCursor();
+        ?>
         <hr>
-        <div class="post-preview">
-          <a href="post.html">
-            <h2 class="post-title">
-              I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.
-            </h2>
-          </a>
-          <p class="post-meta">Posted by
-            <a href="#">Start Bootstrap</a>
-            on September 18, 2019</p>
-        </div>
-        <hr>
-        <div class="post-preview">
-          <a href="post.html">
-            <h2 class="post-title">
-              Science has not yet mastered prophecy
-            </h2>
-            <h3 class="post-subtitle">
-              We predict too much for the next year and yet far too little for the next ten.
-            </h3>
-          </a>
-          <p class="post-meta">Posted by
-            <a href="#">Start Bootstrap</a>
-            on August 24, 2019</p>
-        </div>
-        <hr>
-        <div class="post-preview">
-          <a href="post.html">
-            <h2 class="post-title">
-              Failure is not an option
-            </h2>
-            <h3 class="post-subtitle">
-              Many say exploration is part of our destiny, but it’s actually our duty to future generations.
-            </h3>
-          </a>
-          <p class="post-meta">Posted by
-            <a href="#">Start Bootstrap</a>
-            on July 8, 2019</p>
-        </div>
         <hr>
         <!-- Pager -->
         <div class="clearfix">
-          <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
+          <a class="btn btn-primary float-right" href="#">Billets + anciens &rarr;</a>
         </div>
       </div>
     </div>
--- vues/portions/navigation.php
+++ vues/portions/navigation.php
@@ -1,7 +1,7 @@
 <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container">
-      <a class="navbar-brand" href="homeview.php">Billet simple pour l'Alaska</a>
+      <a class="navbar-brand" href="/index.php">Billet simple pour l'Alaska</a>
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         Menu
         <i class="fas fa-bars"></i>
@@ -9,13 +9,13 @@
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item">
-            <a class="nav-link" href="homeview.php">Accueil</a>
+            <a class="nav-link" href="/index.php">Accueil</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="biographie.php">Biographie</a>
+            <a class="nav-link" href="/vues/biographie.php">Biographie</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="articleview.php">Dernier Chapitre</a>
+            <a class="nav-link" href="/vues/articleview.php">Dernier Chapitre</a>
           </li>
         </ul>
       </div>
