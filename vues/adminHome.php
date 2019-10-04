5de99f3f0c2e26cd407e4cedd469bef1557b864f
Elsa Pioli <elsa.twanss@gmail.com>
2019-10-04 10:41:02 +0200

    Boite de dialogues panneau d'administration

--- vues/adminHome.php
+++ vues/adminHome.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html lang="fr">
 
-<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
+<head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="Billet simple pour l'Alaska, blog de J. Forteroche">
@@ -22,12 +23,12 @@
   <link href="/design/style.css" rel="stylesheet">
 
   <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-  <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-  <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+  <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+  <script type="text/javascript" src="./design/js/jsadmin.js"></script>
 </head>
 
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-<script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
 <?php require_once('portions/navigation.php'); ?>
 
@@ -75,8 +76,8 @@
       <td><?= htmlspecialchars($article['title']); ?></td>
       <td><?= htmlspecialchars($article['content']); ?> </td>
       <td><?php echo $article['date']; ?></td>
-      <td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
-      <td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
+      <td><p data-placement="top" data-toggle="tooltip" title="Modifier"><button class="btn btn-primary btn-xs" data-title="Modifier" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
+      <td><p data-placement="top" data-toggle="tooltip" title="Supprimer"><button class="btn btn-danger btn-xs" data-title="Supprimer" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
     </tr>
     <?php
          }
@@ -115,20 +116,20 @@
     <tbody>
     
          <?php
-                 while ($comment = $allcomments->fetch())
-                 {
-                 ?>
-    <tr>
+          while ($comment = $allcomments->fetch())
+          {
+          ?>
+      <tr>
     
-    <td><?= htmlspecialchars($comment['FK_post']); ?></td>
-    <td><?= htmlspecialchars($comment['author']); ?> </td>
-    <td><?= htmlspecialchars($comment['content']); ?> </td>
-    <td><?php echo $comment['comment_date']; ?></td>
-    <td><?= htmlspecialchars($comment['signalement']); ?> </td>
-    <td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
-    <td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
-    </tr>
-     <?php
+        <td><?= htmlspecialchars($comment['FK_post']); ?></td>
+      <td><?= htmlspecialchars($comment['author']); ?> </td>
+      <td><?= htmlspecialchars($comment['content']); ?> </td>
+      <td><?php echo $comment['comment_date']; ?></td>
+      <td><?= htmlspecialchars($comment['signalement']); ?> </td>
+      <td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Modifier" data-toggle="modal" data-target="#editcomm" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
+      <td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Supprimer" data-toggle="modal" data-target="#deletecomm" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
+      </tr>
+        <?php
          }
          $allcomments->closeCursor();
          ?>
@@ -150,25 +151,19 @@
       <div class="modal-dialog">
     <div class="modal-content">
           <div class="modal-header">
+             <h4 class="modal-title custom_align" id="Heading">Modifier l'article</h4>
         <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
-        <h4 class="modal-title custom_align" id="Heading">Editer l'article</h4>
       </div>
           <div class="modal-body">
           <div class="form-group">
-        <input class="form-control " type="text" placeholder="Mohsin">
+        <input class="form-control " type="text" placeholder="Titre">
         </div>
         <div class="form-group">
-        
-        <input class="form-control " type="text" placeholder="Irshad">
-        </div>
-        <div class="form-group">
-        <textarea rows="2" class="form-control" placeholder="CB 106/107 Street # 11 Wah Cantt Islamabad Pakistan"></textarea>
-    
-        
+        <textarea rows="2" class="form-control" placeholder="Contenu"></textarea>
         </div>
       </div>
           <div class="modal-footer ">
-        <button type="button" class="btn btn-warning btn-lg" style="width: 100%;"><span class="glyphicon glyphicon-ok-sign"></span>Mettre à jour ?</button>
+        <button type="button" class="btn btn-warning btn-lg" style="width: 100%;"><span class="glyphicon glyphicon-ok-sign"></span>Mettre à jour</button>
       </div>
         </div>
     <!-- /.modal-content --> 
@@ -182,17 +177,16 @@
       <div class="modal-dialog">
     <div class="modal-content">
           <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
-        <h4 class="modal-title custom_align" id="Heading">Supprimer l'article ?</h4>
+        <h4 class="modal-title custom_align" id="Heading">Supprimer l'article</h4>
       </div>
           <div class="modal-body">
        
-       <div class="alert alert-danger"><span class="glyphicon glyphicon-warning-sign"></span>Confirmez-vous la suppression ?</div>
+       <div class="alert alert-danger"><span class="glyphicon glyphicon-warning-sign"></span>  Confirmez-vous la suppression ?</div>
        
       </div>
         <div class="modal-footer ">
-        <button type="button" class="btn btn-success" ><span class="glyphicon glyphicon-ok-sign"></span>Oui</button>
-        <button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span>Non</button>
+        <button type="button" class="btn btn-success" ><span class="glyphicon glyphicon-ok-sign"></span> Oui</button>
+        <button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Non</button>
       </div>
         </div>
     <!-- /.modal-content --> 
@@ -201,7 +195,49 @@
     </div>
 
 
-
+<div class="modal fade" id="editcomm" tabindex="-1" role="dialog" aria-labelledby="edit" aria-hidden="true">
+      <div class="modal-dialog">
+    <div class="modal-content">
+          <div class="modal-header">
+             <h4 class="modal-title custom_align" id="Heading">Modifier le commentaire</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
+      </div>
+          <div class="modal-body">
+        <div class="form-group">
+        <textarea rows="2" class="form-control" placeholder="Modifier le commentaire"></textarea>
+        </div>
+      </div>
+          <div class="modal-footer ">
+        <button type="button" class="btn btn-warning btn-lg" style="width: 100%;"><span class="glyphicon glyphicon-ok-sign"></span>Mettre à jour</button>
+      </div>
+        </div>
+    <!-- /.modal-content --> 
+  </div>
+      <!-- /.modal-dialog --> 
+    </div>
+    
+    
+    
+    <div class="modal fade" id="deletecomm" tabindex="-1" role="dialog" aria-labelledby="edit" aria-hidden="true">
+      <div class="modal-dialog">
+    <div class="modal-content">
+          <div class="modal-header">
+        <h4 class="modal-title custom_align" id="Heading">Supprimer le commentaire</h4>
+      </div>
+          <div class="modal-body">
+       
+       <div class="alert alert-danger"><span class="glyphicon glyphicon-warning-sign"></span>  Confirmez-vous la suppression ?</div>
+       
+      </div>
+        <div class="modal-footer ">
+        <button type="button" class="btn btn-success" ><span class="glyphicon glyphicon-ok-sign"></span> Oui</button>
+        <button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Non</button>
+      </div>
+        </div>
+    <!-- /.modal-content --> 
+  </div>
+      <!-- /.modal-dialog --> 
+    </div>
   <hr>
 
   <?php
